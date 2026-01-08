b25e5f9e6a8b101ce98a9de0c44d8f09267f4630
Kristian Antonsen <kba@clearhaus.com>
2026-01-08 16:15:02 +0100

    Use GraphQL update mutations instead of delete-then-create approach
    Problem:
    Updating an action that is referenced by an alert would fail with something like:
    
    │ Error: could not update action: failed to delete existing action: GraphQL errors: [The action cannot be deleted as it is being used by the alert "example_alert_with_labels". (errorCode: CannotDeleteActionItIsBeingUsedByATrigger)]
    │
    │   with humio_action.example_email,
    │   on action_email.tf line 1, in resource "humio_action" "example_email":
    │    1: resource "humio_action" "example_email" {
    │
    ╵
    
    Reproduce:
    1. Create a humio_action resource
    2. Create a humio_alert that references the action
    3. Modify any field on the humio_action
    4. Run terraform apply -> fails with the error above
    
    Fix:
    
    Replace delete-then-create with proper GraphQL update mutations:
    - Added update<Type>ActionMutation for all 8 action types
    - Rewrote Update() to call the appropriate update mutation directly
    - Update mutations modify actions in-place, preserving references

--- internal/api/actions.go
+++ internal/api/actions.go
@@ -355,6 +355,196 @@ mutation CreateWebhookAction(
 }
 `
 
+const updateEmailActionMutation = `
+mutation UpdateEmailAction(
+  $SearchDomainName: String!
+  $ID: String!
+  $Name: String!
+  $Recipients: [String!]!
+  $SubjectTemplate: String
+  $BodyTemplate: String
+  $UseProxy: Boolean!
+) {
+  updateEmailAction(input: {
+    viewName: $SearchDomainName
+    id: $ID
+    name: $Name
+    recipients: $Recipients
+    subjectTemplate: $SubjectTemplate
+    bodyTemplate: $BodyTemplate
+    useProxy: $UseProxy
+  }) {
+    id
+    name
+  }
+}
+`
+
+const updateHumioRepoActionMutation = `
+mutation UpdateHumioRepoAction(
+  $SearchDomainName: String!
+  $ID: String!
+  $Name: String!
+  $IngestToken: String!
+) {
+  updateHumioRepoAction(input: {
+    viewName: $SearchDomainName
+    id: $ID
+    name: $Name
+    ingestToken: $IngestToken
+  }) {
+    id
+    name
+  }
+}
+`
+
+const updateOpsGenieActionMutation = `
+mutation UpdateOpsGenieAction(
+  $SearchDomainName: String!
+  $ID: String!
+  $Name: String!
+  $ApiUrl: String!
+  $GenieKey: String!
+  $UseProxy: Boolean!
+) {
+  updateOpsGenieAction(input: {
+    viewName: $SearchDomainName
+    id: $ID
+    name: $Name
+    apiUrl: $ApiUrl
+    genieKey: $GenieKey
+    useProxy: $UseProxy
+  }) {
+    id
+    name
+  }
+}
+`
+
+const updatePagerDutyActionMutation = `
+mutation UpdatePagerDutyAction(
+  $SearchDomainName: String!
+  $ID: String!
+  $Name: String!
+  $RoutingKey: String!
+  $Severity: String!
+  $UseProxy: Boolean!
+) {
+  updatePagerDutyAction(input: {
+    viewName: $SearchDomainName
+    id: $ID
+    name: $Name
+    routingKey: $RoutingKey
+    severity: $Severity
+    useProxy: $UseProxy
+  }) {
+    id
+    name
+  }
+}
+`
+
+const updateSlackActionMutation = `
+mutation UpdateSlackAction(
+  $SearchDomainName: String!
+  $ID: String!
+  $Name: String!
+  $Url: String!
+  $Fields: [SlackFieldEntryInput!]!
+  $UseProxy: Boolean!
+) {
+  updateSlackAction(input: {
+    viewName: $SearchDomainName
+    id: $ID
+    name: $Name
+    url: $Url
+    fields: $Fields
+    useProxy: $UseProxy
+  }) {
+    id
+    name
+  }
+}
+`
+
+const updateSlackPostMessageActionMutation = `
+mutation UpdateSlackPostMessageAction(
+  $SearchDomainName: String!
+  $ID: String!
+  $Name: String!
+  $ApiToken: String!
+  $Channels: [String!]!
+  $Fields: [SlackFieldEntryInput!]!
+  $UseProxy: Boolean!
+) {
+  updateSlackPostMessageAction(input: {
+    viewName: $SearchDomainName
+    id: $ID
+    name: $Name
+    apiToken: $ApiToken
+    channels: $Channels
+    fields: $Fields
+    useProxy: $UseProxy
+  }) {
+    id
+    name
+  }
+}
+`
+
+const updateVictorOpsActionMutation = `
+mutation UpdateVictorOpsAction(
+  $SearchDomainName: String!
+  $ID: String!
+  $Name: String!
+  $MessageType: String!
+  $NotifyUrl: String!
+  $UseProxy: Boolean!
+) {
+  updateVictorOpsAction(input: {
+    viewName: $SearchDomainName
+    id: $ID
+    name: $Name
+    messageType: $MessageType
+    notifyUrl: $NotifyUrl
+    useProxy: $UseProxy
+  }) {
+    id
+    name
+  }
+}
+`
+
+const updateWebhookActionMutation = `
+mutation UpdateWebhookAction(
+  $SearchDomainName: String!
+  $ID: String!
+  $Name: String!
+  $Url: String!
+  $Method: String!
+  $Headers: [HttpHeaderEntryInput!]!
+  $BodyTemplate: String!
+  $IgnoreSSL: Boolean!
+  $UseProxy: Boolean!
+) {
+  updateWebhookAction(input: {
+    viewName: $SearchDomainName
+    id: $ID
+    name: $Name
+    url: $Url
+    method: $Method
+    headers: $Headers
+    bodyTemplate: $BodyTemplate
+    ignoreSSL: $IgnoreSSL
+    useProxy: $UseProxy
+  }) {
+    id
+    name
+  }
+}
+`
+
 // actionResponse represents the response from list actions query
 type actionResponse struct {
 	SearchDomain struct {
@@ -407,6 +597,17 @@ type createActionResponse struct {
 	CreateSlackPostMessageAction *struct{ ID, Name string } `json:"createSlackPostMessageAction,omitempty"`
 	CreateVictorOpsAction        *struct{ ID, Name string } `json:"createVictorOpsAction,omitempty"`
 	CreateWebhookAction          *struct{ ID, Name string } `json:"createWebhookAction,omitempty"`
+}
+
+type updateActionResponse struct {
+	UpdateEmailAction            *struct{ ID, Name string } `json:"updateEmailAction,omitempty"`
+	UpdateHumioRepoAction        *struct{ ID, Name string } `json:"updateHumioRepoAction,omitempty"`
+	UpdateOpsGenieAction         *struct{ ID, Name string } `json:"updateOpsGenieAction,omitempty"`
+	UpdatePagerDutyAction        *struct{ ID, Name string } `json:"updatePagerDutyAction,omitempty"`
+	UpdateSlackAction            *struct{ ID, Name string } `json:"updateSlackAction,omitempty"`
+	UpdateSlackPostMessageAction *struct{ ID, Name string } `json:"updateSlackPostMessageAction,omitempty"`
+	UpdateVictorOpsAction        *struct{ ID, Name string } `json:"updateVictorOpsAction,omitempty"`
+	UpdateWebhookAction          *struct{ ID, Name string } `json:"updateWebhookAction,omitempty"`
 }
 
 // List returns all actions for the given repository
@@ -640,33 +841,100 @@ func (a *Actions) Add(repository string, action *Action) (*Action, error) {
 	return action, nil
 }
 
-// Update updates an existing action by deleting and recreating it
+// Update updates an existing action in place
 func (a *Actions) Update(repository string, action *Action) (*Action, error) {
-	// First get the existing action to get its ID
-	existing, err := a.Get(repository, action.Name)
+	var resp updateActionResponse
+	var mutation string
+	variables := map[string]interface{}{
+		"SearchDomainName": repository,
+		"ID":               action.ID,
+		"Name":             action.Name,
+	}
+
+	switch action.Type {
+	case ActionTypeEmail:
+		mutation = updateEmailActionMutation
+		variables["Recipients"] = action.EmailAction.Recipients
+		if action.EmailAction.SubjectTemplate != "" {
+			variables["SubjectTemplate"] = action.EmailAction.SubjectTemplate
+		}
+		if action.EmailAction.BodyTemplate != "" {
+			variables["BodyTemplate"] = action.EmailAction.BodyTemplate
+		}
+		variables["UseProxy"] = action.EmailAction.UseProxy
+
+	case ActionTypeHumioRepo:
+		mutation = updateHumioRepoActionMutation
+		variables["IngestToken"] = action.HumioRepoAction.IngestToken
+
+	case ActionTypeOpsGenie:
+		mutation = updateOpsGenieActionMutation
+		variables["ApiUrl"] = action.OpsGenieAction.ApiUrl
+		variables["GenieKey"] = action.OpsGenieAction.GenieKey
+		variables["UseProxy"] = action.OpsGenieAction.UseProxy
+
+	case ActionTypePagerDuty:
+		mutation = updatePagerDutyActionMutation
+		variables["RoutingKey"] = action.PagerDutyAction.RoutingKey
+		variables["Severity"] = action.PagerDutyAction.Severity
+		variables["UseProxy"] = action.PagerDutyAction.UseProxy
+
+	case ActionTypeSlack:
+		mutation = updateSlackActionMutation
+		variables["Url"] = action.SlackAction.Url
+		fields := make([]map[string]string, len(action.SlackAction.Fields))
+		for i, f := range action.SlackAction.Fields {
+			fields[i] = map[string]string{"fieldName": f.FieldName, "value": f.Value}
+		}
+		variables["Fields"] = fields
+		variables["UseProxy"] = action.SlackAction.UseProxy
+
+	case ActionTypeSlackPostMessage:
+		mutation = updateSlackPostMessageActionMutation
+		variables["ApiToken"] = action.SlackPostMessageAction.ApiToken
+		variables["Channels"] = action.SlackPostMessageAction.Channels
+		fields := make([]map[string]string, len(action.SlackPostMessageAction.Fields))
+		for i, f := range action.SlackPostMessageAction.Fields {
+			fields[i] = map[string]string{"fieldName": f.FieldName, "value": f.Value}
+		}
+		variables["Fields"] = fields
+		variables["UseProxy"] = action.SlackPostMessageAction.UseProxy
+
+	case ActionTypeVictorOps:
+		mutation = updateVictorOpsActionMutation
+		variables["MessageType"] = action.VictorOpsAction.MessageType
+		variables["NotifyUrl"] = action.VictorOpsAction.NotifyUrl
+		variables["UseProxy"] = action.VictorOpsAction.UseProxy
+
+	case ActionTypeWebhook:
+		mutation = updateWebhookActionMutation
+		variables["Url"] = action.WebhookAction.Url
+		variables["Method"] = action.WebhookAction.Method
+		headers := make([]map[string]string, len(action.WebhookAction.Headers))
+		for i, h := range action.WebhookAction.Headers {
+			headers[i] = map[string]string{"header": h.Header, "value": h.Value}
+		}
+		variables["Headers"] = headers
+		variables["BodyTemplate"] = action.WebhookAction.BodyTemplate
+		variables["IgnoreSSL"] = action.WebhookAction.IgnoreSSL
+		variables["UseProxy"] = action.WebhookAction.UseProxy
+
+	default:
+		return nil, fmt.Errorf("unsupported action type: %s", action.Type)
+	}
+
+	err := a.client.Query(context.Background(), mutation, variables, &resp)
 	if err != nil {
-		return nil, fmt.Errorf("failed to get existing action: %w", err)
+		return nil, err
 	}
 
-	// Delete the existing action
-	if err := a.Delete(repository, existing.ID); err != nil {
-		return nil, fmt.Errorf("failed to delete existing action: %w", err)
-	}
-
-	// Create the new action
-	return a.Add(repository, action)
+	return action, nil
 }
 
-// Delete deletes an action by name
-func (a *Actions) Delete(repository, actionName string) error {
-	// Look up the action by name to get its ID
-	action, err := a.Get(repository, actionName)
-	if err != nil {
-		return err
-	}
-
+// Delete deletes an action by ID
+func (a *Actions) Delete(repository, actionID string) error {
 	return a.client.Query(context.Background(), deleteActionMutation, map[string]interface{}{
 		"SearchDomainName": repository,
-		"ActionID":         action.ID,
+		"ActionID":         actionID,
 	}, nil)
 }
